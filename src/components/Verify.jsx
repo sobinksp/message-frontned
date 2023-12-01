a819f53e5f4a09075ffdcc11eb4ce1df4165fc63
crb637 <71448044+noxy02@users.noreply.github.com>
2023-12-02 04:22:06 +0700

    Update Verify.jsx

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Verify = () => {
     const { user } = useAuth();
